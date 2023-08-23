3c33ff4f85ed613f780127efe5ce9b9fd47e2919
Gordon Crone <g.crone@ucl.ac.uk>
2023-08-23 13:50:33 +0100

    Add missing src files

---
+++ src/NICReceiver.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file NICReceiver.cpp
+ *
+ * Implementation of NICReceiver's dal methods
+ *
+ * This is part of the DUNE DAQ Software Suite, copyright 2023.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "readoutdal/DROStreamConf.hpp"
+#include "readoutdal/EthStreamParameters.hpp"
+#include "readoutdal/NICReceiver.hpp"
+
+#include "readoutdalIssues.hpp"
+
+using namespace dunedaq::readoutdal;
+
+std::string NICReceiver::get_ipaddress() {
+  auto streams = get_streams();
+  if (streams.size() == 0) {
+    return "";
+  }
+  auto streamPars = streams[0]->get_stream_params();
+  auto ethPars = streamPars->cast<EthStreamParameters>();
+  if (streamPars == nullptr) {
+    throw (BadStreamConf(ERS_HERE, streamPars->UID(), "EthStreamParameters"));
+  }
+  return ethPars->get_rx_ip();
+}
+
+std::string NICReceiver::get_macaddress() {
+  auto streams = get_streams();
+  if (streams.size() == 0) {
+    return "";
+  }
+  auto streamPars = streams[0]->get_stream_params();
+  auto ethPars = streamPars->cast<EthStreamParameters>();
+  if (streamPars == nullptr) {
+    throw (BadStreamConf(ERS_HERE, streamPars->UID(), "EthStreamParameters"));
+  }
+  return ethPars->get_rx_mac();
+}
---
+++ src/readoutdalIssues.hpp
@@ -0,0 +1,10 @@
+#include "ers/Issue.hpp"
+
+namespace dunedaq {
+  ERS_DECLARE_ISSUE(readoutdal, BadConf, what, ((std::string)what))
+  ERS_DECLARE_ISSUE(readoutdal, BadStreamConf,
+                    "Failed to cast stream parameters " << id << " to " << stype,
+                    ((std::string)id) ((std::string)stype))
+}
+
+
