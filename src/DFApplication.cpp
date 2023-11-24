afefe53f8dc9836dc412862de445165be72d40bf
roland-sipos <sipos.roland.cb@gmail.com>
2023-11-24 16:32:48 +0100

    dfapp gen modules with 1 trb 1 dwr, queue for trigger records and net conn for Fragments

--- src/DFApplication.cpp
+++ src/DFApplication.cpp
@@ -14,6 +14,10 @@
 #include "oks/kernel.hpp"
 #include "coredal/Connection.hpp"
 #include "coredal/NetworkConnection.hpp"
+#include "appdal/TRBuilder.hpp"
+#include "appdal/TRBConf.hpp"
+#include "appdal/DataWriter.hpp"
+#include "appdal/DataWriterConf.hpp"
 #include "appdal/DFApplication.hpp"
 #include "appdal/NetworkConnectionRule.hpp"
 #include "appdal/NetworkConnectionDescriptor.hpp"
@@ -39,6 +43,23 @@ __reg__("DFApplication", [] (const SmartDaqApplication* smartApp,
   }
   );
 
+inline void
+fill_queue_object_from_desc(const QueueDescriptor* qDesc, oksdbinterfaces::ConfigObject& qObj)
+{
+  qObj.set_by_val<std::string>("data_type", qDesc->get_data_type());
+  qObj.set_by_val<std::string>("queue_type", qDesc->get_queue_type());
+  qObj.set_by_val<uint32_t>("capacity", qDesc->get_capacity());
+}
+
+inline void
+fill_netconn_object_from_desc(const NetworkConnectionDescriptor* netDesc, oksdbinterfaces::ConfigObject& netObj)
+{
+  netObj.set_by_val<std::string>("data_type", netDesc->get_data_type());
+  netObj.set_by_val<std::string>("connection_type", netDesc->get_connection_type());
+  netObj.set_by_val<std::string>("uri", netDesc->get_uri());
+  netObj.set_by_val<uint16_t>("port", netDesc->get_port());
+}
+
 std::vector<const coredal::DaqModule*> 
 DFApplication::generate_modules(oksdbinterfaces::Configuration* confdb,
                                      const std::string& dbfile,
@@ -46,8 +67,78 @@ DFApplication::generate_modules(oksdbinterfaces::Configuration* confdb,
 {
   std::vector<const coredal::DaqModule*> modules;
 
-  // Code to actually generate modules should go here
-  std::cout << __FUNCTION__ << " -- Unimplemented function\n";
+  auto sessionApps = session->get_all_applications();
+  for (auto app : sessionApps) {
+    TLOG() << "app in session: " << app;
+  }
+
+  // Process the queue rules looking for the TriggerRecord queue between TRB and DataWriter
+  const QueueDescriptor* trQDesc = nullptr;
+  for (auto rule : get_queue_rules()) {
+    auto destination_class = rule->get_destination_class();
+    if (destination_class == "DataWriter") {
+      trQDesc = rule->get_descriptor();
+    }
+  }
+  if (trQDesc == nullptr) { // BadConf if no descriptor between TRB and DataWriter
+    throw (BadConf(ERS_HERE, "Could not find queue descriptor rule for TriggerRecords!"));
+  }
+  // Create queue connection config object
+  oksdbinterfaces::ConfigObject trQueueObj;
+  std::string trQueueUid("trigger-records-"+std::to_string(1));
+  confdb->create(dbfile, "Queue", trQueueUid, trQueueObj);
+  fill_queue_object_from_desc(trQDesc, trQueueObj);
+
+  // Process the network rules looking for the Fragments input for TRB
+  const NetworkConnectionDescriptor* fragNetDesc = nullptr;
+  for (auto rule : get_network_rules()) {
+    auto endpoint_class = rule->get_endpoint_class();
+    auto descriptor = rule->get_descriptor();
+    auto data_type = descriptor->get_data_type();
+    if (endpoint_class == "TRBuilder" && data_type == "Fragment") {
+      fragNetDesc = rule->get_descriptor();
+    }
+  }
+  if (fragNetDesc == nullptr) { // BadConf if no descriptor for Fragments into TRB
+    throw (BadConf(ERS_HERE, "Could not find queue descriptor rule for input Fragments!"));
+  }
+  // Create network connection config object
+  oksdbinterfaces::ConfigObject fragNetObj;
+  std::string fragNetUid("fragments-to-dataflow-"+std::to_string(1));
+  confdb->create(dbfile, "NetworkConnection", fragNetUid, fragNetObj);
+  fill_netconn_object_from_desc(fragNetDesc, fragNetObj);
+
+  // Get TRB Config Object
+  auto trbConf = get_trb();
+  if (trbConf == 0) {
+    throw (BadConf(ERS_HERE, "No DataWriter or TRB configuration given"));
+  }
+  auto trbConfObj = trbConf->config_object();
+  // Prepare TRB Module Object and assign its Config Object.
+  oksdbinterfaces::ConfigObject trbObj;
+  std::string trbUid("trbuilder-"+std::to_string(1));
+  confdb->create(dbfile, "TRBuilder", trbUid, trbObj);
+  trbObj.set_obj("configuration", &trbConfObj);
+  trbObj.set_objs("inputs", {&fragNetObj});
+  trbObj.set_objs("outputs", {&trQueueObj});
+  // Push TRB Module Object from confdb
+  modules.push_back(confdb->get<TRBuilder>(trbUid));
+
+
+  // Get DataWriter Config Object
+  auto dwrConf = get_data_writer();
+  if (dwrConf == 0) {
+    throw (BadConf(ERS_HERE, "No DataWriter or TRB configuration given"));
+  }
+  auto dwrConfObj = dwrConf->config_object();
+  // Prepare DataWriter Module Object and assign its Config Object.
+  oksdbinterfaces::ConfigObject dwrObj;
+  std::string dwrUid("datawriter-"+std::to_string(1));
+  confdb->create(dbfile, "DataWriter", dwrUid, dwrObj);
+  dwrObj.set_obj("configuration", &dwrConfObj);
+  dwrObj.set_objs("inputs", {&trQueueObj});
+  // Push DataWriter Module Object from confdb
+  modules.push_back(confdb->get<DataWriter>(dwrUid));
 
   return modules;
 }
--- src/TPWriterApplication.cpp
+++ src/TPWriterApplication.cpp
@@ -15,6 +15,8 @@
 #include "coredal/Connection.hpp"
 #include "coredal/NetworkConnection.hpp"
 #include "appdal/TPWriterApplication.hpp"
+#include "appdal/TPWriterConf.hpp"
+#include "appdal/TPWriter.hpp"
 #include "appdal/NetworkConnectionRule.hpp"
 #include "appdal/NetworkConnectionDescriptor.hpp"
 #include "appdal/QueueConnectionRule.hpp"
@@ -24,6 +26,7 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace dunedaq;
 using namespace dunedaq::appdal;
@@ -46,8 +49,49 @@ TPWriterApplication::generate_modules(oksdbinterfaces::Configuration* confdb,
 {
   std::vector<const coredal::DaqModule*> modules;
 
-  // Code to actually generate modules should go here
-  std::cout << __FUNCTION__ << " -- Unimplemented function\n";
+  auto tpwriterConf = get_tp_writer();
+  if (tpwriterConf == 0) {
+    throw (BadConf(ERS_HERE, "No TPWriter configuration given"));
+  }
+  auto tpwriterConfObj = tpwriterConf->config_object();
+  auto dataStoreParams = tpwriterConf->get_data_store_params();
+
+  // Get TPSet sources
+/*
+  auto roMap = session->get_readout_map();
+  for (auto roGroup : roMap->get_groups()) {
+    if (roGroup->disabled(*session)) {
+      TLOG() << "Ignoring disabled ReadoutGroup " << roGroup->UID();
+      continue; 
+    }
+    auto rset = roGroup->cast<coredal::ReadoutGroup>();
+    if (rset == nullptr) {
+      throw (BadConf(ERS_HERE, "TPWriterApplication contains something other than ReadoutGroup"));
+    }
+  }
+*/
+
+  //oksdbinterfaces::ConfigObject 
+  std::string tpNetUid("SubToTPH-"+std::to_string(1));
+
+  oksdbinterfaces::ConfigObject tpwrObj;
+  std::string tpwrUid("tpwriter-"+std::to_string(100));
+  confdb->create(dbfile, "TPWriter", tpwrUid, tpwrObj);
+  tpwrObj.set_obj("configuration", &tpwriterConf->config_object());
+  //tpwrObj.set_obj("inputs", {&} );
+
+
+  modules.push_back(confdb->get<TPWriter>(tpwrUid));
+
+  // Process the queue rules looking for inputs to our TPWriter module
+  const QueueDescriptor *tpwrInputQDesc = nullptr;
+  for (auto rule : get_queue_rules()) {
+    //auto destination_class = rule->get_destination_class();
+    // if (destination_class)
+    std::cout << "RULE is: " << rule << '\n'; 
+  }
+
+  
 
   return modules;
 }
