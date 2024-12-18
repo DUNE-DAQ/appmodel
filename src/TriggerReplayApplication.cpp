c6184d6ba8f9239d2cb649bade4374a627da018d
MRiganSUSX <M.Rigan@sussex.ac.uk>
2024-12-18 19:38:15 +0100

    replay setup for multi-files

--- src/TriggerReplayApplication.cpp
+++ src/TriggerReplayApplication.cpp
@@ -90,46 +90,31 @@ TriggerReplayApplication::generate_modules(conffwk::Configuration* confdb,
     tph_class = tph_conf->get_template_for();
   }
 
+  // For now, have X identical config TP Handlers
+  // X = either num of files if < 4; or 4
+  // Later, do this dynamically, but that requires opening the HDF5s...
+  int max_APAs = tpmm_conf->get_tp_streams().size();
+  int APA_limit = std::min(max_APAs, 4);
+  std::vector<std::shared_ptr<conffwk::ConfigObject>> TPHs;
+  std::vector<std::string> TPHs_uids;
+
   auto tph_conf_obj = tph_conf->config_object();
-  conffwk::ConfigObject tph_obj;
-  std::string tp_uid("tphandler-replay-1");
-  confdb->create(dbfile, tph_class, tp_uid, tph_obj);
-  tph_obj.set_by_val<uint32_t>("source_id", 0);
-  tph_obj.set_by_val<uint32_t>("detector_id", 1); // 1 == kDAQ
-  tph_obj.set_by_val<bool>("post_processing_enabled", true);
-  tph_obj.set_obj("module_configuration", &tph_conf_obj);
-
-  auto tph2_conf_obj = tph_conf->config_object();
-  conffwk::ConfigObject tph2_obj;
-  std::string tp2_uid("tphandler-replay-2");
-  confdb->create(dbfile, tph_class, tp2_uid, tph2_obj);
-  tph2_obj.set_by_val<uint32_t>("source_id", 1);
-  tph2_obj.set_by_val<uint32_t>("detector_id", 1); // 1 == kDAQ
-  tph2_obj.set_by_val<bool>("post_processing_enabled", true);
-  tph2_obj.set_obj("module_configuration", &tph2_conf_obj);
-
-  auto tph3_conf_obj = tph_conf->config_object();
-  conffwk::ConfigObject tph3_obj;
-  std::string tp3_uid("tphandler-replay-3");
-  confdb->create(dbfile, tph_class, tp3_uid, tph3_obj);
-  tph3_obj.set_by_val<uint32_t>("source_id", 2);
-  tph3_obj.set_by_val<uint32_t>("detector_id", 1); // 1 == kDAQ
-  tph3_obj.set_by_val<bool>("post_processing_enabled", true);
-  tph3_obj.set_obj("module_configuration", &tph3_conf_obj);
-
-  auto tph4_conf_obj = tph_conf->config_object();
-  conffwk::ConfigObject tph4_obj;
-  std::string tp4_uid("tphandler-replay-4");
-  confdb->create(dbfile, tph_class, tp4_uid, tph4_obj);
-  tph4_obj.set_by_val<uint32_t>("source_id", 3);
-  tph4_obj.set_by_val<uint32_t>("detector_id", 1); // 1 == kDAQ
-  tph4_obj.set_by_val<bool>("post_processing_enabled", true);
-  tph4_obj.set_obj("module_configuration", &tph_conf_obj);
+  for (int i = 1; i <= APA_limit; i++) {
+    auto tph_obj = std::make_shared<conffwk::ConfigObject>();
+    std::string tp_uid = "tphandler-replay-" + std::to_string(i);
+    TPHs_uids.push_back(tp_uid);
+    confdb->create(dbfile, tph_class, tp_uid, *tph_obj);
+    tph_obj->set_by_val<uint32_t>("source_id", i);
+    tph_obj->set_by_val<uint32_t>("detector_id", 1); // 1 == kDAQ
+    tph_obj->set_by_val<bool>("post_processing_enabled", true);
+    tph_obj->set_obj("module_configuration", &tph_conf_obj);
+    TPHs.push_back(tph_obj);
+  }
 
   /**************************************************************
-  * Deal with queues
-  **************************************************************/
-  // load queue confs
+   * Deal with queues
+   **************************************************************/
+  // Load queue configurations
   const QueueDescriptor* tp_inputq_desc = nullptr;
 
   for (auto rule : get_queue_rules()) {
@@ -140,63 +125,84 @@ TriggerReplayApplication::generate_modules(conffwk::Configuration* confdb,
     }
   }
 
-  // setup queue objects
-  conffwk::ConfigObject tp_iq1_obj;
-  std::string tp_iq1_uid("tpinput-1");
-  confdb->create(dbfile, "QueueWithSourceId", tp_iq1_uid, tp_iq1_obj);
-  tp_iq1_obj.set_by_val<std::string>("data_type", tp_inputq_desc->get_data_type());
-  tp_iq1_obj.set_by_val<std::string>("queue_type", tp_inputq_desc->get_queue_type());
-  tp_iq1_obj.set_by_val<uint32_t>("capacity", tp_inputq_desc->get_capacity());
-  tp_iq1_obj.set_by_val<uint32_t>("source_id", 0);
+  // Same as above, later dynamically
+  std::vector<std::shared_ptr<conffwk::ConfigObject>> TP_queues;
+  for (int i = 1; i <= APA_limit; i++) {
+    auto tp_q_obj = std::make_shared<conffwk::ConfigObject>();
+    std::string tp_q_uid = "tpinput-" + std::to_string(i);
+    confdb->create(dbfile, "QueueWithSourceId", tp_q_uid, *tp_q_obj);
+    tp_q_obj->set_by_val<std::string>("data_type", tp_inputq_desc->get_data_type());
+    tp_q_obj->set_by_val<std::string>("queue_type", tp_inputq_desc->get_queue_type());
+    tp_q_obj->set_by_val<uint32_t>("capacity", tp_inputq_desc->get_capacity());
+    tp_q_obj->set_by_val<uint32_t>("source_id", i);
+    TP_queues.push_back(tp_q_obj);
+  }
 
   /**************************************************************
-  * Deal with network connections
-  **************************************************************/
+   * Deal with network connections
+   **************************************************************/
   const NetworkConnectionDescriptor* ta_net_desc = nullptr;
   const NetworkConnectionDescriptor* dr_net_desc = nullptr;
-  
-  for (auto rule : get_network_rules()) {  
+
+  for (auto rule : get_network_rules()) {
     auto endpoint_class = rule->get_endpoint_class();
     auto data_type = rule->get_descriptor()->get_data_type();
     if (data_type == "TriggerActivity") {
       ta_net_desc = rule->get_descriptor();
-    }
-    else if (data_type == "DataRequest") {
-      dr_net_desc = rule->get_descriptor(); 
+    } else if (data_type == "DataRequest") {
+      dr_net_desc = rule->get_descriptor();
     }
   }
 
-  conffwk::ConfigObject ta_net_obj;
-  auto ta_service_obj = ta_net_desc->get_associated_service()->config_object();
-  std::string ta_stream_uid(ta_net_desc->get_uid_base()+UID());
-  confdb->create(dbfile, "NetworkConnection", ta_stream_uid, ta_net_obj);
-  ta_net_obj.set_by_val<std::string>("data_type", ta_net_desc->get_data_type());
-  ta_net_obj.set_by_val<std::string>("connection_type", ta_net_desc->get_connection_type());
-  ta_net_obj.set_obj("associated_service", &ta_service_obj);
+  // Create vectors for network connections
+  std::vector<std::shared_ptr<conffwk::ConfigObject>> ta_net_objects;
+  std::vector<std::shared_ptr<conffwk::ConfigObject>> dr_net_objects;
 
-  conffwk::ConfigObject dr_net_obj;
-  auto dr_service_obj = dr_net_desc->get_associated_service()->config_object();
-  std::string dr_stream_uid(dr_net_desc->get_uid_base()+UID());
-  confdb->create(dbfile, "NetworkConnection", dr_stream_uid, dr_net_obj);
-  dr_net_obj.set_by_val<std::string>("data_type", dr_net_desc->get_data_type());
-  dr_net_obj.set_by_val<std::string>("connection_type", dr_net_desc->get_connection_type());
-  dr_net_obj.set_obj("associated_service", &dr_service_obj);
- 
+  for (int i = 1; i <= APA_limit; i++) {
+    auto ta_net_obj = std::make_shared<conffwk::ConfigObject>();
+    auto ta_service_obj = ta_net_desc->get_associated_service()->config_object();
+    std::string ta_stream_uid = ta_net_desc->get_uid_base() + UID() + "-" + std::to_string(i);
+    confdb->create(dbfile, "NetworkConnection", ta_stream_uid, *ta_net_obj);
+    ta_net_obj->set_by_val<std::string>("data_type", ta_net_desc->get_data_type());
+    ta_net_obj->set_by_val<std::string>("connection_type", ta_net_desc->get_connection_type());
+    ta_net_obj->set_obj("associated_service", &ta_service_obj);
+    ta_net_objects.push_back(ta_net_obj);
+
+    auto dr_net_obj = std::make_shared<conffwk::ConfigObject>();
+    auto dr_service_obj = dr_net_desc->get_associated_service()->config_object();
+    std::string dr_stream_uid = dr_net_desc->get_uid_base() + UID() + "-" + std::to_string(i);
+    confdb->create(dbfile, "NetworkConnection", dr_stream_uid, *dr_net_obj);
+    dr_net_obj->set_by_val<std::string>("data_type", dr_net_desc->get_data_type());
+    dr_net_obj->set_by_val<std::string>("connection_type", dr_net_desc->get_connection_type());
+    dr_net_obj->set_obj("associated_service", &dr_service_obj);
+    dr_net_objects.push_back(dr_net_obj);
+  }
+
   /**************************************************************
-  * Finally set inputs & outputs
-  **************************************************************/
+   * Finally set inputs & outputs
+   **************************************************************/
+  // Convert TP_queues to a vector of raw pointers
+  std::vector<const conffwk::ConfigObject*> raw_tp_queues;
+  for (const auto& tp_queue : TP_queues) {
+    raw_tp_queues.push_back(tp_queue.get());
+  }
+  tpm_obj.set_objs("outputs", raw_tp_queues);
 
-  tpm_obj.set_objs("outputs", {&tp_iq1_obj} );
-  //tpm_obj.set_objs("outputs", {&tp_iq1_obj, &tp_iq2_obj, &tp_iq3_obj, &tp_iq4_obj} ); 
-  tph_obj.set_objs("inputs", {&tp_iq1_obj, &dr_net_obj} );
-  tph_obj.set_objs("outputs", {&ta_net_obj} );
+  for (int i = 1; i <= APA_limit; i++) {
+    // Convert network objects to raw pointers
+    std::vector<const conffwk::ConfigObject*> temp_inputs = {
+      TP_queues[i - 1].get(),
+      dr_net_objects[i - 1].get()
+    };
+    TPHs[i - 1]->set_objs("inputs", temp_inputs);
+    TPHs[i - 1]->set_objs("outputs", { ta_net_objects[i - 1].get() });
+  }
 
-  // store modules
+  // Store modules
   modules.push_back(confdb->get<confmodel::DaqModule>(tpmm_conf->UID()));
-  modules.push_back(confdb->get<confmodel::DaqModule>(tp_uid));
-  //modules.push_back(confdb->get<confmodel::DaqModule>(tp2_uid));
-  //modules.push_back(confdb->get<confmodel::DaqModule>(tp3_uid));
-  //modules.push_back(confdb->get<confmodel::DaqModule>(tp4_uid));
+  for (int i = 1; i <= APA_limit; i++) {
+    modules.push_back(confdb->get<confmodel::DaqModule>(TPHs_uids[i - 1]));
+  }
 
   return modules;
 }
