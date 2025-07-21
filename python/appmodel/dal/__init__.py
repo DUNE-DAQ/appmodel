131bb216682792d9afebc07902d082acd5775c4e
John Freeman <jcfree@daq.fnal.gov>
2025-07-21 16:22:27 -0500

    JCF: DUNE-DAQ/daq-cmake#156: update the __init__.py files to work with changes to daq_add_python_bindings

---
+++ python/appmodel/dal/__init__.py
@@ -0,0 +1,13 @@
+from ._daq_appmodel_dal_py import *
+
+__all__= ['generate_modules']
+
+class UnknownGeneratorException(Exception):
+    pass
+
+def generate_modules(confdb, app, session):
+    
+    mods = smart_dap_application_generate_modules(confdb._obj, app.id, session.id)
+
+    return [confdb.get_dal(m.class_name, m.id) for m in mods]
+
