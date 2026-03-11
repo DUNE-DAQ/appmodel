d658f83b0f584ff5f06bcf50368c140feecc0694
Deniz Tuana Ergonul Uzun <deniz.tuana.ergonul@cern.ch>
2026-03-11 16:14:56 +0100

    Don't check raw data callbacks for CRT readers

--- test/apps/generate_modules_test.cxx
+++ test/apps/generate_modules_test.cxx
@@ -27,8 +27,9 @@
 
 #include "appmodel/DataHandlerModule.hpp"
 #include "appmodel/DataReaderModule.hpp"
+#include "appmodel/CRTBernReaderModule.hpp"
+#include "appmodel/CRTGrenobleReaderModule.hpp"
 #include "appmodel/DataMoveCallbackConf.hpp"
-#include "appmodel/SocketDataWriterModule.hpp"
 
 #include "appmodel/appmodelIssues.hpp"
 
@@ -97,26 +98,21 @@ main(int argc, char* argv[])
 
       auto reader_module = daq_module->cast<appmodel::DataReaderModule>();
       if (reader_module != nullptr) {
-        auto callback_confs = reader_module->get_raw_data_callbacks();
-        std::cout << " callback confs " << std::endl;
-        for (auto* callback_conf : callback_confs) {
-          auto cbObj = callback_conf->config_object();
-          cbObj.print_ref(std::cout, *confdb, "    ");
+        auto crt_reader_module = daq_module->cast<dunedaq::appmodel::CRTBernReaderModule>() != nullptr ||
+                                 daq_module->cast<dunedaq::appmodel::CRTGrenobleReaderModule>() != nullptr;
+        if (!crt_reader_module) { // Don't check raw data callbacks for CRT readers
+          auto callback_confs = reader_module->get_raw_data_callbacks();
+          std::cout << " callback confs " << std::endl;
+          for (auto* callback_conf : callback_confs) {
+            auto cbObj = callback_conf->config_object();
+            cbObj.print_ref(std::cout, *confdb, "    ");
+          }
         }
       }
 
       auto handler_module = daq_module->cast<appmodel::DataHandlerModule>();
       if (handler_module != nullptr) {
         auto callback_conf = handler_module->get_raw_data_callback();
-        if (callback_conf != nullptr) {
-          auto cbObj = callback_conf->config_object();
-          cbObj.print_ref(std::cout, *confdb, "    ");
-        }
-      }
-
-      auto socketwriter_module = daq_module->cast<appmodel::SocketDataWriterModule>();
-      if (socketwriter_module != nullptr) {
-        auto callback_conf = socketwriter_module->get_raw_data_callback();
         if (callback_conf != nullptr) {
           auto cbObj = callback_conf->config_object();
           cbObj.print_ref(std::cout, *confdb, "    ");
